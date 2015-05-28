82e3014423939fea467c61750833aaccf53ece4b
Kevin5025 <Kevin5025@yahoo.com>
2015-05-28 00:06:44 -0400

    prototype main menu

---
+++ Assets/MainMenuButtons.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtons : MonoBehaviour {
+	
+	string username;
+	public UnityEngine.UI.InputField inputUsername;
+	public UnityEngine.UI.Text textUsername;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//username = inputUsername.text;
+		//textUsername.text = username;
+	}
+
+	public void StartGame () {
+		Application.LoadLevel (1);
+	}
+
+	public void QuitGame () {
+		//Debug.Log ("Quit");
+		Application.Quit ();
+	}
+	
+	public void UsernameConfirm () {
+		username = inputUsername.text;
+		textUsername.text = username;
+		//inputUsername.enabled = false;//can only set once
+	}
+}
