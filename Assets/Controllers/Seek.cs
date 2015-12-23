bab0f2853be33cf57a82aee1aa9c63fac1ca0636
Nick Werle <nick.werle96@gmail.com>
2015-12-23 12:06:53 -0500

    Seek now forces rotation to target
    Next, this needs to use physics

---
+++ Assets/Controllers/Seek.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seek : MonoBehaviour {
+    public GameObject target;
+    public float distance = 0;
+	
+    // Update is called once per frame
+	void Update () {
+        var dir = target.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+}
